8e04635172e7fba02b997f37f48c96b5a04cc0fb
Chris Morrell <inxilpro@users.noreply.github.com>
2021-03-22 21:14:51 -0400

    Clean up

--- src/main/kotlin/com/github/inxilpro/intellijalpine/Alpine.kt
+++ src/main/kotlin/com/github/inxilpro/intellijalpine/Alpine.kt
@@ -4,29 +4,4 @@ import com.intellij.openapi.util.IconLoader
 
 object Alpine {
     val ICON = IconLoader.getIcon("/META-INF/pluginIcon.svg", Alpine::class.java)
-
-    val DIRECTIVES = arrayOf(
-        "x-data",
-        "x-init",
-        "x-show",
-        "x-model",
-        "x-text",
-        "x-html",
-        "x-ref",
-        "x-if",
-        "x-for",
-        "x-transition",
-        "x-spread",
-        "x-cloak",
-    )
-
-    val EVENT_PREFIXES = arrayOf(
-        "@",
-        "x-on:"
-    )
-
-    val BIND_PREFIXES = arrayOf(
-        ":",
-        "x-bind:"
-    )
 }
